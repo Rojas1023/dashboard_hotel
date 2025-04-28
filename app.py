211c4a6e86aeed8a5137f7d58377d1702aea8fc2
Jhoan Rojas <157655074+Rojas1023@users.noreply.github.com>
2025-04-27 22:55:59 -0500

    Update app.py import eventlet eventlet.monkey_patch()

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_socketio import SocketIO, emit
 from werkzeug.utils import secure_filename
